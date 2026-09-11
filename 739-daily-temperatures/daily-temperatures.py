525d2d1ba51984cfa19dea94a9a9584c01f6e068
Shiven Singh <ss.shiven44@gmail.com>
2026-09-11 01:19:01 +0100

    Time: 91 ms (66.43%) | Memory: 28.7 MB (47.73%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.py
@@ -0,0 +1,16 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        result = [0] * len(temperatures)  #jab bhi koi position ka element fix karna ho
+        stack = []
+        for i in range(len(temperatures)):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                popped_value = stack.pop()
+                result[popped_value] = (i - popped_value)
+            stack.append(i)
+        return result
+
+
+
+            
+            
+
