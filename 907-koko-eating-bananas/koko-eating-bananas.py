07645f9386086948d2c88cab7ebd25ff7865cfa2
Shiven Singh <ss.shiven44@gmail.com>
2026-09-11 20:17:36 +0100

    Time: 167 ms (63.53%) | Memory: 20.8 MB (13.07%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,16 +1,15 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         l,r = 1, max(piles)
-        res = r
+        final_speed = float('inf')
         while l<=r:
-            k = (l+r)//2
-            hours = 0
+            mid = l + ((r-l)//2)
+            sum_of_hours = 0
             for i in piles:
-                hours += math.ceil(i/k)
-            if hours<=h:
-                res = min(res,k)
-                r=k-1
-    
+                sum_of_hours += math.ceil(i/mid)
+            if sum_of_hours <= h:
+                final_speed = min(mid, final_speed)
+                r = mid - 1
             else:
-                l=k+1
-        return res
+                l = mid + 1
+        return final_speed
