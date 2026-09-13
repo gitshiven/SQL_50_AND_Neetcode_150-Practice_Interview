97eeffe553e26b30b85f32bd412239f8f61fa817
Shiven Singh <ss.shiven44@gmail.com>
2026-09-13 13:52:54 +0100

    Time: 1 ms (86.38%) | Memory: 19.3 MB (81.79%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.py
+++ 445-add-two-numbers-ii/add-two-numbers-ii.py
@@ -1,29 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        stack_1 = []        # ← 8 spaces
-        curr = l1
-        while curr:
-            stack_1.append(curr.val)
-            curr = curr.next
-
+        stack_1 = []
         stack_2 = []
-        curr = l2
-        while curr:
-            stack_2.append(curr.val)
-            curr = curr.next
-
+        cur1 = l1
+        cur2 = l2
+        while cur1:
+            stack_1.append(cur1.val)
+            cur1 = cur1.next
+        while cur2:
+            stack_2.append(cur2.val)
+            cur2 = cur2.next
+       
         carry = 0
         head = None
+        #Now add
         while stack_1 or stack_2 or carry:
-            val1 = stack_1.pop() if stack_1 else 0
-            val2 = stack_2.pop() if stack_2 else 0
+            val_1 = stack_1.pop() if stack_1 else 0
+            val_2 = stack_2.pop() if stack_2 else 0
 
-            total = val1 + val2 + carry
-            carry = total // 10
-            digit = total % 10
-
-            node = ListNode(digit)
-            node.next = head
-            head = node
-
+            total = val_1 + val_2 + carry
+            carry = total//10
+            digit = total%10
+        
+            Node = ListNode(digit)
+            Node.next = head
+            head = Node
         return head
